239b1c00cd7ed8a50a5ff876e562443813598310
ishafiq95 <hamsting95@gmail.com>
2021-08-19 13:43:27 +0800

    Added few JS scripts and modal box
    - Created a working menu button when viewed in mobile/tablet mode
    
    - Added modal box when user clicks the 'Sign up' menu

--- js/main.js
+++ js/main.js
@@ -17,3 +17,56 @@
  * 1) Make the responsive menu work!
  * 2) Implement a modal window - "Sign up now" CTA when someone clicks the 'Sign Up' button
  */ 
+
+
+//Easy Level Tasks
+//1) Get the title for each dog/cat services, and put them into the console using console.log
+console.log(document.querySelector(".rover-hero-title").innerText)
+console.log(document.querySelector(".rover-123-container-title").innerText)
+
+//2) Change the title of the page to "We're the CAT People"
+const textChange = "We are cat person. Sorry doggie"
+
+document.querySelector(".rover-hero-title").innerText = textChange
+document.querySelector(".rover-123-container-title").innerText = textChange
+
+//3) Change the main image to be something cat related.
+document.querySelector(".rover-hero-container").style.backgroundImage = "url('https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg')"
+
+// ==================================================================================================
+
+//Hard Level Tasks
+//1) Replace the call to action button background color with "red"
+document.querySelector(".rover-button").style.backgroundColor = "red"
+document.querySelector(".rover-cta-mobile").style.backgroundColor = "red"
+document.querySelector(".rover-cta-btn").style.backgroundColor = "red"
+
+//2) Find another image and replace the header (try unsplash.com, get familiar with searching for images)
+document.querySelector(".rover-header-image").src = "https://cdn.iconscout.com/icon/premium/png-256-thumb/cat-paw-961585.png"
+document.querySelector(".rover-header-image").width = 80 
+
+//3) When someone clicks the "Book your next Rover sitter" button, change the text of the button to say "Thanks"
+document.querySelector(".rover-cta-btn").addEventListener("click", function() {
+    document.querySelector(".rover-cta-btn").innerText = "Thank you"
+})
+
+// ==================================================================================================
+
+//Hardest Level Tasks
+//1) Make the responsive menu work!
+document.querySelector(".rover-header-right-mobile").addEventListener("click", function() {
+    document.querySelector(".rover-header-right-mobile-menu").style.display = "block"
+})
+
+document.querySelector(".menu-mobile-close-btn").addEventListener("click", function() {
+    document.querySelector(".rover-header-right-mobile-menu").style.display = "none"
+})
+
+//2) Implement a modal window - "Sign up now" CTA when someone clicks the 'Sign Up' button
+document.querySelector(".rover-header-right > a").addEventListener("click", function() {
+    document.querySelector(".modal-box-container").style.display = "flex"
+})
+
+document.querySelector(".modal-box-container .modal-box .modal-box-close-button > a").addEventListener("click", function() {
+    document.querySelector(".modal-box-container").style.display = "none"
+})
